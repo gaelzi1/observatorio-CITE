c05aa489f4e9987361afd29657dd0b10826ccec0
unknown <mbrito31@alumnos.uaq.mx>
2026-08-18 10:24:41 -0600

    vesion 1.4 add S

--- app/articles/[slug]/page.jsx
+++ app/articles/[slug]/page.jsx
@@ -6,7 +6,7 @@ import ArticleDetailSkeleton from "@/components/ArticleDetailSkeleton"
 
 export default async function ArticlePage({ params }) {
   const resolvedParams = await params;
-  const slug = resolvedParams?.slug || resolvedParams?.id;
+  const {slug} = await params.slug;
   return(
   <><Header />
   <ArticleDetail slug={slug} />
