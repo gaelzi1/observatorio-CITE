4799f7c407b3fdbaaf0289e36bf31fed33ba00f5
unknown <mbrito31@alumnos.uaq.mx>
2026-08-18 10:27:26 -0600

    vesion 1.4 add S

--- app/articles/[slug]/page.jsx
+++ app/articles/[slug]/page.jsx
@@ -1,15 +1,18 @@
 import ArticleDetail from "@/components/ArticleDetail";
 import Header from "@/components/Header";
-import ArticleDetailSkeleton from "@/components/ArticleDetailSkeleton"
+import ArticleDetailSkeleton from "@/components/ArticleDetailSkeleton";
 
-
+// 1. Agregamos esta línea para obligar a Next.js a renderizar esto en tiempo real
+export const dynamic = 'force-dynamic';
 
 export default async function ArticlePage({ params }) {
-  const resolvedParams = await params;
-  const {slug} = await params.slug;
-  return(
-  <><Header />
-  <ArticleDetail slug={slug} />
-  </>
-); 
+  // 2. Esperamos los parámetros correctamente
+  const { slug } = await params;
+
+  return (
+    <>
+      <Header />
+      <ArticleDetail slug={slug} />
+    </>
+  ); 
 }
